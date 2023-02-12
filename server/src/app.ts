1be47206d787c6a3b9bdd859ea7c5716ab335525
B.D.Heshan Madhuranga <39455525+heshanm27@users.noreply.github.com>
2023-02-12 11:08:40 +0530

    init

---
+++ server/src/app.ts
@@ -0,0 +1,14 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import { logger } from "./util";
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+dotenv.config();
+
+app.listen(process.env.PORT || 8000, () => {
+  logger.info(`Server is running on port ${process.env.PORT || 8000}`);
+  console.log("Server is running on port 3000");
+});
