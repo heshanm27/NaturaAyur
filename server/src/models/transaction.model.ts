2b4b6b77a41acf00c7046b21a60b4d3fbe10e7aa
B.D.Heshan Madhuranga <39455525+heshanm27@users.noreply.github.com>
2023-02-13 22:13:51 +0530

    added schemas

---
+++ server/src/models/transaction.model.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const TransactionSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+    status: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true },
+    country: { type: String, required: true },
+    paymentMethod: { type: String, required: true },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    taxPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
+    isPaid: { type: Boolean, required: true, default: false },
+    paidAt: { type: Date },
+    isDelivered: { type: Boolean, required: true, default: false },
+    deliveredAt: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+);
