7082e380a8d200bce4f7988ec34545079d9d13ad
Ruchira Ranasinghe <it20068028@my.sliit.lk>
2023-04-22 12:21:14 +0530

    products restructured
    products restructured

---
+++ backend/products/src/service/product.service.ts
@@ -0,0 +1,13 @@
+export async function createProduct(input: any) {}
+
+export async function patchProduct(input: any) {}
+
+export async function removeProduct(input: any) {}
+
+export async function findAllProducts(input: any) {}
+
+export async function findProductBySellerId(input: any) {}
+
+export async function findProductByCategory(input: any) {}
+
+export async function getAllReviewsForProduct(input: any) {}
