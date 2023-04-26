6b0e2f64e7108e2f27687a5c647207540ce52472
Ruchira Ranasinghe <it20068028@my.sliit.lk>
2023-04-26 12:13:46 +0530

    orders_restructure
    added dependency files from the product directory

--- backend/orders/src/routes/order.routes.ts
+++ backend/orders/src/routes/order.routes.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import { addOrder, deleteOrder, getAllOrderList, getLiveOrder, getOneOrder, getOrderHistory, patchOrderStatus } from "../controller/order.controller";
 
 const Router = express.Router();
 
 //default routes
-Router.route("/").get().post();
+Router.route("/").get(getAllOrderList).post(addOrder);
 
+Router.route("/live").get(getLiveOrder);
+Router.route("/history").get(getOrderHistory);
 //paramterized routes
-Router.route("/:id").get().patch().delete();
+Router.route("/:id").get(getOneOrder).patch(patchOrderStatus).delete(deleteOrder);
 
 export default Router;
